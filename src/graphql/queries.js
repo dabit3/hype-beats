cbe415729bdf69fa15a902ca2c171b1eac8bd6a1
Dabit <dabit@8c85901b6e1c.ant.amazon.com>
2019-03-19 05:36:26 -0700

    updated limit

--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -16,7 +16,7 @@ export const listDrumMachines = gql`query ListDrumMachines(
   $limit: Int
   $nextToken: String
 ) {
-  listDrumMachines(filter: $filter, limit: 500, nextToken: $nextToken) {
+  listDrumMachines(filter: $filter, limit: $limit, nextToken: $nextToken) {
     items {
       id
       clientId
